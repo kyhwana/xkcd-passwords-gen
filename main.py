5878ba7b0c1ccd1c293e41de55e0326edc2dc0c6
kyhwana <kyhwana@gmail.com>
2014-06-29 15:26:16 +1200

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+__author__ = 'kyhwana'
+
+#TODO: take dictionary as command line argument, \n separated
+#TODO: output full x-y words from dictionary (variable, 2-5, etc)
+#TODO: output MD5/SHA1/other hashes of output sentences
+#TODO: Add modifiers between words, " ", "_" etc.
+#TODO: randomly generate sentances, specify how many to generate.
+#TODO: add max output length. Make max output length either cut off words or not output sentences more than <x>
+#TODO: Output to stdout vs output to file.
+import os
